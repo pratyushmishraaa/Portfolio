a61c5a4f5b37e5a3a81e977e9b374ab57e32ad54
Pratyush Mishra <pratyushmishra287@gmail.com>
2022-08-21 22:50:32 +0530

    Add files via upload

---
+++ sampleportfolio-main/app.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+var text = document.getElementById("text");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
+    } else {
+        icon.src = "images/moon.png";
+    }
+};
